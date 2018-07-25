dda1005a7cc4cda89b772cc576513f11a7fa6e7e
DPaulReed <39937785+DPaulReed@users.noreply.github.com>
2018-07-25 16:18:06 -0500

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,31 @@
+<?php
+$message = "PHP is working";
+/*echo "<script type=\"text/javascript\"> alert(\"$message\"); </script>";*/
+?>
+<div class="main-header">
+<div class="resume">
+eResume
+</div>
+<div class="me">
+<strong>D o n a l d   R e e d</strong><br> 
+10780 Allen Drive<br>
+Pass Christian, MS  39571 US<br>
+Phone: 228-365-6512<br>
+Email: <a href="mailto:Donald.Reed@usm.edu">Donald.Reed@usm</a><br>
+</div>
+<div><image class="profile" src="../images/me.png" /></div>
+<div>
+<image class="contactqr" src="../images/contactqr.png" />
+</div>
+</div>
+<div class="menubar">
+<button class="button"  onclick="location.href='#Ex'">Experience</button>
+<button class="button"  onclick="location.href='#Ed'">Education</button>
+<button class="button"  onclick="location.href='#Train'">Training</button>
+<button class="button"  onclick="location.href='#Summary'">Summary</button>
+<button class="button"  onclick="location.href='#Skills'">Skills</button>
+<button class="button"  onclick="location.href='contact.php'">Contact Me</button>
+<span class="right"><button target="_blank" class="button" onclick="window.open('https://github.com/dpaulreed/resume','_blank')">Github</button></span>
+<span class="right"><button target="_blank" class="button" onclick="window.open('https://www.linkedin.com/in/donald-reed-23480315a/','_blank')">Linkedin</button></span>
+<span class="right"><button target="_blank" class="button" onclick="window.open('https://trello.com/donaldreed','_blank')">Trello</button></span>
+</div>
