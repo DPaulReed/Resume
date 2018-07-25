926fb9486f5cd9383438cd1df9ce93a3b96fc218
DPaulReed <39937785+DPaulReed@users.noreply.github.com>
2018-07-25 22:34:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,17 +8,17 @@
 </head>
 
 <body background="images/bg_layer_0.png"> 
-<div id="layer1"><img class="bg" src="../images/bg_layer_1.png" />
+<div id="layer1"><img class="bg" src="images/bg_layer_1.png" />
 <h1> Donald Reed</h1>
 <button class="button"  onclick="location.href='resume.php'">Click to Enter</button>
 </div><div class="logo1"> SOFTWARE ENGINEER </div><div class="logo2"> ELECTRONICS TECHNICIAN </div><div class="logo3"> PROJECT MANAGER </div>
-<img class="ring1" src="../images/ring_1.png" />
-<img class="ring2" src="../images/ring_2.png" />
-<img class="ring3" src="../images/ring_3.png" />
-<img class="ring4" src="../images/ring_4.png" />
-<img class="ring5" src="../images/ring_5.png" />
-<img class="ring6" src="../images/ring_6.png" />
-<img class="ring7" src="../images/ring_7.png" />
+<img class="ring1" src="images/ring_1.png" />
+<img class="ring2" src="images/ring_2.png" />
+<img class="ring3" src="images/ring_3.png" />
+<img class="ring4" src="images/ring_4.png" />
+<img class="ring5" src="images/ring_5.png" />
+<img class="ring6" src="images/ring_6.png" />
+<img class="ring7" src="images/ring_7.png" />
 </body>
 
 </html>
